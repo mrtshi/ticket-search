17766c28512e163e69c4732c82a6b3aa4c3b202b
mrtshi <168927415+mrtshi@users.noreply.github.com>
2026-07-13 15:18:05 +0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { TicketSearch } from "@/components/ticket-search";
+
+export default function HomePage() {
+  return (
+    <div className="container mx-auto px-4 py-12">
+      <TicketSearch />
+    </div>
+  );
+}
