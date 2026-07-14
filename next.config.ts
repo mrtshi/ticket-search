9792b48081ac347f8d82ae767cff6e7f8de13199
mrtshi <168927415+mrtshi@users.noreply.github.com>
2026-07-14 09:22:25 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,9 @@
 import type { NextConfig } from "next";
 
-const basePath =
-  process.env.BASE_PATH !== undefined ? process.env.BASE_PATH : "";
-
 const nextConfig: NextConfig = {
-  output: process.env.OUTPUT_MODE === "export" ? "export" : undefined,
-
-  ...(basePath && {
-    basePath,
-    assetPrefix: basePath,
-  }),
-
-  ...(process.env.OUTPUT_MODE === "export" && {
-    images: {
-      unoptimized: true,
-    },
-  }),
-  allowedDevOrigins: ["*"],
-
-  devIndicators: false,
-  poweredByHeader: false,
-  reactStrictMode: true,
+  serverActions: {
+    bodySizeLimit: "50mb",
+  },
 };
 
 export default nextConfig;
