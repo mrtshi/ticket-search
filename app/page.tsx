fb4f4e81f76c8b77b5aec0270fcbfc7b1e4a0be5
mrtshi <168927415+mrtshi@users.noreply.github.com>
2026-07-14 16:44:51 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
 "use client";
 
-import { TicketSearch } from "@/components/ticket-search";
+import TicketSearch from "@/components/ticket-search";
 
 export default function HomePage() {
-  return (
-    <div className="container mx-auto px-4 py-12">
-      <TicketSearch />
-    </div>
-  );
+  return <TicketSearch />;
 }
